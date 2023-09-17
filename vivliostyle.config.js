497f5c8f263e433048b0f6611f3afb78c2ca7233
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-09-17 16:24:45 +0900

    執筆準備

---
+++ vivliostyle.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: "mybook",
+  author: "都立桜修館中等教育学校 75期記念祭幹部 会計班",
+  size: "a5",
+  language: "ja",
+  theme: "themes/mytheme",
+  entry: ["maegaki.md", "honbun.md", "atogaki.md"],
+  entryContext: "./manuscripts/",
+};
